d6db46b5513f3ab84de3f3916f818a5fdfb6ea49
unknown <jv179@wowshga.internal>
2017-08-25 18:38:03 +0800

    a

--- v1_a5e27b11/index.js
+++ v1_a5e27b11/index.js
@@ -14,19 +14,33 @@ function Seed(opts) {
     var self = this,
         root = this.el = document.getElementById(opts.id),
         els = root.querySelectorAll(selector)
-    bindings = {}
+        bindings = {}
 
     self.scope = {}
 
+
+    
     var scopeArr = opts.scope && Object.keys(opts.scope)
 
     for (var i = 0; i < scopeArr.length; i++) {
         self.scope[scopeArr[i]] = opts.scope[scopeArr[i]]
     }
 
-    function bind(variable){
-        ;[].forEach.call(self.scope,function(){
+    // var bindingsArr = Object.keys(selector)
 
+    // for(var i = 0; i<bindingsArr.length;i++){
+
+    // }
+
+
+    function bind(variable){
+        // ;[].forEach.call(self.scope,function(){
+
+        // })
+        Object.defineProperty(self.scope,variable,{
+            set:function(newVal){
+
+            }
         })
     }
 
